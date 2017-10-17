42f13fa40a0dfa6f245d086c8f32e58a855efc41
jonathan Morton <jmorton4193@gmail.com>
2017-10-17 18:40:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+ var sel=document.querySelectorAll(selector)
+return sel[0]
+}
+
+function nestedTarget (){
+  var sel = document.getElementById('nested')
+return sel
+}
+
+function increaseRankBy (n){
+  var current=document.querySelectorAll(parseInt('ul.ranked-list'))
+    for(let i=0; i<current.length; i++){
+    var  next=[]
+      next.pop(current[i])
+      next+=n
+    }
+
+    return next
+}
+
+function deepestChild (){
+          var current=document.getElementById('grand-node'.div)
+var store=[]
+store.push(current)
+      return current
+  }
